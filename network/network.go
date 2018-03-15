1cf3a8ecabe634eb51c030ef0917a12e0393be91
devplayg <devplayg@gmail.com>
2018-03-15 14:46:30 +0900

    added some network functions

--- network/network.go
+++ network/network.go
@@ -11,34 +11,3 @@ func IpToInt32(ip net.IP) uint32 {
 	}
 	return binary.BigEndian.Uint32(ip)
 }
-
-func GetIpv4CIDRRange(s string) (net.IP, net.IP, error) {
-	list, err := GetIpv4CIDRRangeList(s)
-	if err != nil {
-		return nil, nil, err
-	}
-	return list[0], list[len(list)-1], nil
-}
-
-func GetIpv4CIDRRangeList(s string) ([]net.IP, error) {
-	ip, ipNet, err := net.ParseCIDR(s)
-	if err != nil {
-		return nil, err
-	}
-
-	list := make([]net.IP, 0)
-	for ip := ip.Mask(ipNet.Mask); ipNet.Contains(ip); inc(ip) {
-		list = append(list, net.ParseIP(ip.String()))
-	}
-	return list, err
-}
-
-// https://play.golang.org/p/oJcoGMJngcE
-func inc(ip net.IP) {
-	for j := len(ip) - 1; j >= 0; j-- {
-		ip[j]++
-		if ip[j] > 0 {
-			break
-		}
-	}
-}
