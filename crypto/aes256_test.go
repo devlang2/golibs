73c244516dcfe77e20afbdff2adfc99fe6a42ccb
devplayg <devplayg@gmail.com>
2020-05-18 10:01:48 +0900

    need to fix aes256

--- crypto/aes256_test.go
+++ crypto/aes256_test.go
@@ -2,9 +2,7 @@ package crypto
 
 import (
 	"bytes"
-	"crypto/aes"
 	"fmt"
-	"github.com/davecgh/go-spew/spew"
 	"io/ioutil"
 	"os"
 	"reflect"
@@ -13,57 +11,6 @@ import (
 
 func p(str string) {
 	fmt.Println("######################### " + str + " #########################")
-}
-func TestDecAes256(t *testing.T) {
-	data := []byte("devplayg")
-	p("data")
-	spew.Dump(data)
-	key := []byte("heeloheeheeloheeheeloheeheelohee")
-
-	p("key")
-	spew.Dump(key)
-	// fmt.Printf("key len=%d\n", len(key))
-	encData, err := aesCBCEncrypt(data, key[:])
-	if err != nil {
-		t.Error(err)
-	}
-
-	p("enc")
-	spew.Dump(encData)
-	decData, err := aesCBCDncrypt(encData, key[:])
-	if err != nil {
-		t.Error(err)
-	}
-
-	if !bytes.Equal(data, decData) {
-		t.Error(err)
-	}
-
-	block, _ := aes.NewCipher(key[:])
-	iv := encData[:block.BlockSize()]
-	encData2, err := _aesCBCEncrypt(data, key[:], iv)
-	if err != nil {
-		t.Error(err)
-	}
-	p("enc2")
-	spew.Dump(encData2)
-	if !bytes.Equal(encData, encData2) {
-		t.Error(err)
-	}
-
-	// EncAes256()
-	encData3, err := EncAes256Old(key[:], iv, data)
-	if err != nil {
-		t.Error()
-	}
-
-	if !bytes.Equal(encData, encData3) {
-		t.Error(err)
-		p("enc3")
-		spew.Dump(encData3)
-	}
-	// spew.Dump(data)
-	// spew.Dump(decData)
 }
 
 func TestEncrypt(t *testing.T) {
